3566c8fd2f387814361923fe92e6c798aa1e0fbf
Kylian <Kylian.crohem@le-campus-numerique.fr>
2023-06-23 11:35:22 +0200

    modification Kylian

--- index.php
+++ index.php
@@ -6,14 +6,37 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
           integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
           crossorigin="">
+    <link rel="stylesheet" href="load.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <title>Title</title>
 </head>
 <body>
+<div class="loads">
+<svg class="loading" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     width="574.558px" height="190px" viewBox="0 0 574.558 120" enable-background="new 0 0 574.558 120" xml:space="preserve">
+  <defs>
+      <pattern id="water" width=".25" height="1.1" patternContentUnits="objectBoundingBox">
+          <path fill="#fff" d="M0.25,1H0c0,0,0-0.659,0-0.916c0.083-0.303,0.158,0.334,0.25,0C0.25,0.327,0.25,1,0.25,1z"/>
+      </pattern>
+
+      <text id="text" transform="matrix(1 0 0 1 -8.0684 116.7852)" font-size="125">ERROR 404</text>
+
+      <mask id="text_mask">
+          <use x="0" y="0" xlink:href="#text" opacity="1" fill="#3498db"/>
+      </mask>
+  </defs>
+
+    <use x="0" y="0" xlink:href="#text" fill="#3498db"/>
+
+    <rect class="water-fill" mask="url(#text_mask)" fill="url(#water)" x="-400" y="0" width="1600" height="120"/>
+</svg>
+<script src="load.js"></script>
+</div>
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container-fluid">
+            <img class="top img-fluid" src="img/logoHeader.png"  alt="logo MOKU">
             <a class="navbar-brand">Home</a>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
@@ -22,7 +45,6 @@
                     </li>
                 </ul>
             </div>
-            <img class="top" src="img/logoHeader.png" alt="logo MOKU">
             <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Rechercher ...">
                 <button type="button" class="btn btn-outline-success">Entrée</button>
--- main.js
+++ main.js
@@ -4,10 +4,37 @@ var CartoDB_VoyagerLabelsUnder = L.tileLayer('https://{s}.basemaps.cartocdn.com/
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 20
-});
+}).addTo(map);
+
+coords = [[45.89856023085219, 6.117736246509577], [45.90706560732963, 6.113352659053238] , [45.91763558459165, 6.128630520636682]];
+rent = ['Station', 'Station' , 'Station'];
+//areas
+noms = ["Prix carburant" , "Prix carburant", "Prix carburant" ]
+// rooms
+prix = ["1.25" , "2.89" , "5"]
+//outside
 
 
-CartoDB_VoyagerLabelsUnder.addTo(map)
+let l = coords.length;
+
+for (let i = 0; i < l; i++){
+    //popus
+    var pop = L.popup({
+        closeOnClick: true
+    }).setContent /* affiche ce mot ('Station essence');*/('<h2>nom : ' + noms[i] + ' prix: ' + prix[i]);
+    //marqueur
+
+    var marker = L.marker(coords[i]).addTo(map).bindPopup(pop);
+
+    //labels
+    var toollip = L.tooltip({
+        permanent: true
+    }).setContent(rent[i]);
+
+    marker.bindTooltip(toollip);
+}
+
+
 
 /* MODE DARK
 var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
@@ -16,10 +43,13 @@ var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/a
 });
 Stadia_AlidadeSmoothDark.addTo(map)*/
 
+/* ping
 var marker = L.marker([45.89856023085219, 6.117736246509577]).addTo(map);
 
 var popup = L.popup();
+*/
 
+/* Message a afficher
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
@@ -28,3 +58,4 @@ function onMapClick(e) {
 }
 
 map.on('click', onMapClick);
+*/
